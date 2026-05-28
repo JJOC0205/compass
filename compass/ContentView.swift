c76cab277600ac1ec3526167e4b7f6c38378ff09
Jon O'Connell <jjoc0205@gmail.com>
2026-05-27 22:56:30 -0400

    Start of simple compass

--- compass/ContentView.swift
+++ compass/ContentView.swift
@@ -8,7 +8,49 @@
 import SwiftUI
 import SwiftData
 
+struct Marker: Hashable{
+    let degrees: Double
+    let label: String
+    
+    init(degrees: Double, label: String = ""){
+        self.degrees = degrees
+        self.label = label
+    }
+    
+    static func markers() -> [Marker] {
+        return [
+            Marker(degrees: 0, label: "N"),
+            Marker(degrees: 30),
+            Marker(degrees: 60),
+            Marker(degrees: 90, label: "E"),
+            Marker(degrees: 150),
+            Marker(degrees: 180, label: "S"),
+            Marker(degrees: 210),
+            Marker(degrees: 240),
+            Marker(degrees: 270, label: "W"),
+            Marker(degrees: 300),
+            Marker(degrees: 330)
+        ]
+    }
+}
+
 struct ContentView: View {
+    var body: some View {
+        VStack {
+            Capsule()
+                .frame(width: 5, height: 50)
+            
+            // 1
+            ZStack{
+                //2
+                ForEach([0,30,60,90,120,150,180,210,240,270,300,330], id: \.self) { marker in // TODO
+                }
+            }.frame(width: 300, height: 300)
+                .rotationEffect(Angle(degrees: 0))
+                .statusBar(hidden: true)
+        }
+    }
+    /*
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
 
@@ -53,6 +95,8 @@ struct ContentView: View {
             }
         }
     }
+     
+     */
 }
 
 #Preview {
